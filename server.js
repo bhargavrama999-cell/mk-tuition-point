68fb94862a7916c72bf6d5c268444ea25fef5979
bhargavrama999-cell <bhargavrama999@gmail.com>
2026-04-16 10:22:59 +0530

    tenth commit

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
-app.options('*', cors()); // Enable pre-flight for all routes
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
